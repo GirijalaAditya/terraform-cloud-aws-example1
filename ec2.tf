d30bd78c881d663b48a71da0f4ec4b5ba8d33909
Aditya Girijala <aditya.girijala>
2023-09-22 18:21:00 +0530

    ec2 tags

--- ec2.tf
+++ ec2.tf
@@ -4,5 +4,9 @@ resource "aws_instance" "instance_1" {
   key_name = var.key_name
   user_data = file("${path.module}/script.sh")
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
-  tags = local.common_tags
+  tags = {
+    Name = "Instance-1"
+    Environment = local.environment
+    "Business_Division" = local.business_division
+  }
 }
