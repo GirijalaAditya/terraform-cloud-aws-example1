9691be28b7e6b39713104c56e443d953b861f4aa
Aditya Girijala <aditya.girijala>
2023-09-22 18:40:11 +0530

    s3 website

--- s3.tf
+++ s3.tf
@@ -11,3 +11,13 @@ resource "aws_s3_bucket_versioning" "s3_bucket_vers" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_website_configuration" "s3_website" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  index_document {
+    suffix = "s3-websites-files/index.html"
+  }
+  error_document {
+    key = "s3-websites-files/error.html"
+  }
+}
