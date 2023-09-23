9bd21dae0a3efa62540e9b89ad97ded4a4785d7d
Aditya Girijala <aditya.girijala>
2023-09-23 23:56:26 +0530

    aws s3 policy

--- main.tf
+++ main.tf
@@ -32,8 +32,8 @@ resource "aws_s3_bucket_policy" "name" {
         "s3:GetObject"
       ],
       "Resource": [
-		    aws_s3_bucket.s3_bucket.arn,
-		    "${aws_s3_bucket.s3_bucket.arn}/*",
+		    "${aws_s3_bucket.s3_bucket.arn}",
+		    "${aws_s3_bucket.s3_bucket.arn}/*"
 	    ]
     }
   ]
